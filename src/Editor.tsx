54fd149f6560fcf6d79cf5daae5b3c1b3ba39232
matsuo-keita <matsukeiprog@gmail.com>
2022-11-20 19:26:24 +0900

    Refactoring

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -12,8 +12,18 @@ const Editor = () => {
 const EditorArea = () => {
   const [texts, setText] = useState(["","|"]);
   const [row, setRow] = useState(1);
+
   const buttonClick = (event:any) =>{
-    const char = event.target.innerHTML;
+    let char:string;
+    if(event.target.innerHTML === "&lt;"){
+      char = "<";
+    }else if(event.target.innerHTML === "&gt;"){
+      char = ">";
+    }else if(event.target.innerHTML === "&amp;"){
+      char = "&";
+    }else{
+      char = event.target.innerHTML;
+    }
     texts[row] = texts[row].slice(0, -1) + char + "|"
     setText([...texts]);
   }
@@ -30,39 +40,32 @@ const EditorArea = () => {
     texts[row] = texts[row].slice(0, -1)
     setText([...texts,"|"]);
   }
-  const buttonsRow1:string[] = ["q","w","e","r","t","y","u","i","o","p","@","["]
-  const buttonsRow2 = ["a","s","d","f","g","h","j","k","l",";",":","]"]
-  const buttonsRow3 = ["z","x","c","v","b","n","m",",",".","/","_"]
-  const buttonsRow4 = ["!","\"","#","$","%","&","'","(",")","-","=","~"]
-  const buttonsRow5 = ["{","}","<",">","?","`","|","^"]
-  const buttonsRow6 = ["←","↑","→","↓"]
+  const buttons = [
+    ["q","w","e","r","t","y","u","i","o","p"],
+    ["a","s","d","f","g","h","j","k","l"],
+    ["z","x","c","v","b","n","m"],
+    ["(",")","{","}","<",">","[","]"],
+    ["=","&","|","!","?","+","-","*","/","%","^"],
+    [";",".","\"",",",":","'","`","_","#","$","~","@"]
+  ]
   return (
     <div>
       <div className="editor-line">1 {texts[1]}</div>
       <div>2 {texts[2]}</div>
       <div>3 {texts[3]}</div>
-      <div>Run</div>
-      <div className="row1">
-        { buttonsRow1.map((item,index) => <button onClick={buttonClick} key={index}>{item}</button>) }
-      </div>
-      <div className="row2">
-        { buttonsRow2.map((item,index) => <button onClick={buttonClick} key={index+100}>{item}</button>) }
-      </div>
-      <div className="row3">
-        { buttonsRow3.map((item,index) => <button onClick={buttonClick} key={index+200}>{item}</button>) }
-      </div>
-      <div className="row4">
-        { buttonsRow4.map((item,index) => <button onClick={buttonClick} key={index+300}>{item}</button>) }
-      </div>
-      <div className="row5">
-        { buttonsRow5.map((item,index) => <button onClick={buttonClick} key={index+400}>{item}</button>) }
-      </div>
+      <button className="run-button">Run</button>
+      { buttons.map((buttonRow,rowIndex) => 
+        <div>
+         {buttonRow.map((item,index) => <button onClick={buttonClick} key={ 100 * rowIndex + index}>{item}</button>)}
+        </div>)
+      }
       <button onClick={buttonDelete}>Delete</button>
       <button onClick={buttonSpace}>Space</button>
       <button onClick={buttonEnter}>Enter</button>
-      <div className="row6">
-        { buttonsRow6.map((item,index) => <button onClick={buttonClick} key={index+500}>{item}</button>) }
-      </div>
+      <button onClick={buttonEnter}>←</button>
+      <button onClick={buttonEnter}>↑</button>
+      <button onClick={buttonEnter}>→</button>
+      <button onClick={buttonEnter}>↓</button>
     </div>
   );
 }
